ae783471af180696ab3ae27408dd06c8712e8beb
Ricky Pan <ricky.pan@utp.ac.pa>
2022-11-27 18:14:38 -0500

    team
    jalar el contenido de team a una lista

---
+++ teams.php
@@ -0,0 +1,13 @@
+<?php
+include('dbcon.php');
+
+    $sql = "SELECT * FROM team";
+    $result = $con_bd -> query($sql);
+    if($result -> num_rows > 0){
+        echo "<ol>";
+        while($row = $result -> fetch_assoc()){
+            echo "<li>".$row["name"]."</li>";
+         }
+         echo "</ol>";
+    }
+?>
