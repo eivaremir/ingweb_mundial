975d45c6a8db7d02425e0a69d4c78ad834798f0e
Ricky Pan <ricky.pan@utp.ac.pa>
2022-11-28 00:21:35 -0500

    datos
    se agregan los datos de los partidos jugados por el equipo

--- team.php
+++ team.php
@@ -10,6 +10,7 @@ echo "<br>";
     if($result -> num_rows > 0){
         while($row = $result -> fetch_assoc()){
             if ($codigo == $row['code']){
+                $nombre = $row["name"];
                 $id = $row["id"];
                 echo $row["name"]. "<br><br>";
             }
@@ -21,9 +22,41 @@ $sql = "SELECT * FROM team_match inner join team on team_match.team = team.id wh
     if($result -> num_rows > 0){
         while($row = $result -> fetch_assoc()){
             echo "Juego:" .$row["match"]. "<br>";
-            echo "Goles:" .$row["goals"]. "<br><br>";
+            echo "Equipo:" .$nombre. "<br>";
+            echo "Goles:" .$row["goals"]. "<br>";
+            $equipo = $row["team"];
+            $juego = $row["match"];
+            enemigo($equipo, $juego);
          }
     }
+
+    function enemigo($equipo, $juego){
+    include('queries/dbcon.php');
+
+    $sql = "SELECT * FROM team_match";
+    $result = $con_bd -> query($sql);
+    if($result -> num_rows > 0){
+        while($row = $result -> fetch_assoc()){
+            if($juego== $row['match'] && $equipo != $row['team']){
+                $gol = $row["goals"];
+                $enemigo = $row["team"];
+
+                $sql = "SELECT * FROM team";
+                $result = $con_bd -> query($sql);
+                if($result -> num_rows > 0){
+                    while($row = $result -> fetch_assoc()){
+                        if ($enemigo == $row['id']){
+                            echo "Contrincantes:" .$row["name"]. "<br>";
+                            echo "Goles del contrincante:" .$gol. "<br><br>";
+                        }
+                    }
+                }
+            }
+         }
+    }
+}
+
+
 
     $sql = "SELECT * FROM player";
     $result = $con_bd -> query($sql);
@@ -36,5 +69,7 @@ $sql = "SELECT * FROM team_match inner join team on team_match.team = team.id wh
          }
          echo "</ol>";
     }
+
+
 
 ?>
