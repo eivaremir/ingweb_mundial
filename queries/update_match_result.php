d53394be6a4fcedd0212878ef289b62a595ff41e
Eivar Morales <eivaremir@gmail.com>
2022-11-28 21:49:07 -0500

    changed var name

--- queries/update_match_result.php
+++ queries/update_match_result.php
@@ -5,9 +5,9 @@ include("dbcon.php");
 $result_id = $_GET["id"];
 $new_value = $_GET["value"];
 
-$insert_user_query = "update team_match set goals = " . $new_value . " where id=" . $result_id . ";";
+$update_goals_query = "update team_match set goals = " . $new_value . " where id=" . $result_id . ";";
 try {
-    if ($con_bd->query($insert_user_query)) {
+    if ($con_bd->query($update_goals_query)) {
         echo '
             { "result":"success"}
         ';
