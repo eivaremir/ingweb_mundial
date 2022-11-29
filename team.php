b04ec108abbc37bfef0c3c5060e78890f8965075
Eivar Morales <eivaremir@gmail.com>
2022-11-29 13:22:10 -0500

    improved team design

--- team.php
+++ team.php
@@ -37,24 +37,41 @@
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             if ($codigo == $row['code']) {
-                echo "Logros:" .$row["achivements"]. "<br><br>";
+
+                echo "
+
+                <div class='team-details-container'>
+                    <div class='team-summary'>
+                    <div class='team-flag' style='background-image:url(https://cloudinary.fifa.com/api/v3/picture/flags-sq-2/" . $row["code"] . "?tx=c_fill,g_auto,q_auto,w_1000,h_650)'></div> 
+                    <h2>" . $row["code"] . "</h2>
+                    <h3>" . $row["name"] . "</h3>
+                    </div>
+                    <div class='team-quote'>
+                    <h2>Logros</h2>
+                        " . $row["achivements"] . "
+                    </div>
+                
+                
+                ";
+
+
             }
         }
     }
-    
-    $sql = "SELECT * FROM team_match inner join team on team_match.team = team.id where team.code= '$codigo'";
-    $result = $con_bd->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "Juego:" . $row["match"] . "<br>";
-            echo "Equipo:" . $nombre . "<br>";
-            echo "Goles:" . $row["goals"] . "<br>";
-            $equipo = $row["team"];
-            $juego = $row["match"];
-            enemigo($equipo, $juego);
-        }
-    }
 
+    // $sql = "SELECT * FROM team_match inner join team on team_match.team = team.id where team.code= '$codigo'";
+    // $result = $con_bd->query($sql);
+    // if ($result->num_rows > 0) {
+    //     while ($row = $result->fetch_assoc()) {
+    //         echo "Juego:" . $row["match"] . "<br>";
+    //         echo "Equipo:" . $nombre . "<br>";
+    //         echo "Goles:" . $row["goals"] . "<br>";
+    //         $equipo = $row["team"];
+    //         $juego = $row["match"];
+    //         enemigo($equipo, $juego);
+    //     }
+    // }
+    
     function enemigo($equipo, $juego)
     {
         include('queries/dbcon.php');
@@ -87,6 +104,8 @@
     $sql = "SELECT * FROM player";
     $result = $con_bd->query($sql);
     if ($result->num_rows > 0) {
+        echo "<div>";
+        echo "<h2>Jugadores</h2>";
         echo "<ol>";
         while ($row = $result->fetch_assoc()) {
             if ($id == $row['team']) {
@@ -94,11 +113,12 @@
             }
         }
         echo "</ol>";
+        echo "</div>";
     }
 
+    echo "</div>"
 
-
-    ?>
+        ?>
 
 </body>
 
